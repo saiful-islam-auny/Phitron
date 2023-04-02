0c7dbbef61460502bf7b774291aad3cc2b0936d5
saiful-islam-auny <aunychowdhury99@gmail.com>
2023-04-02 22:24:23 +0600

    module 01 modified

---
+++ C/module_01/input_output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d%% %d%%\n",a,b);
+
+
+    scanf("%da %db",&a,&b);
+    printf("%da %db",a,b);
+
+    return 0;
+}
