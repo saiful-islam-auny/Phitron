a06cadbf5bf1d01dc0a8117143f3c1686a127e4d
saiful-islam-auny <aunychowdhury99@gmail.com>
2023-04-05 14:46:42 +0600

    folder name modified

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 0;
+    
+    if (x)//x==true
+    {
+        printf("hi");
+    }
+    else
+        printf("how are you");
+    
+    return 0;
+}
