86849b6669eb3e601452d714a2659b6ca50df621
saiful-islam-auny <aunychowdhury99@gmail.com>
2023-05-07 12:33:47 +0600

    scope in c

--- C/module 14/scope.c
+++ C/module 14/scope.c
@@ -4,18 +4,20 @@ int x=500;
 void fun(void)
 {
     // printf("fun er x er address - %p\n",&x);
+    x = 10;
+    printf("fun x er value - %d\n",x);
 }
 int main()
 {
     // printf("Main er x er address - %p\n",&x);
 
-    // fun();
+    fun();
 
-    int y=100,i;
-    for( i=0;i<5;i++)
-    {
-        int z=100;
-    }
-    printf("%d",z);
+    // int y=100,i;
+    // for( i=0;i<5;i++)
+    // {
+    //     int z=100;
+    // }
+    printf("main er x: %d",x);
     return 0;
 }
