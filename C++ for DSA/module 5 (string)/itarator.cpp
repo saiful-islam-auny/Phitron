3d95f3276b95529f71c07f50c5f382e5c0481e89
saiful-islam-auny <aunychowdhury99@gmail.com>
2023-05-29 21:57:05 +0600

    module 3.5 & 5 updated

--- C++ for DSA/module 5 (string)/itarator.cpp
+++ C++ for DSA/module 5 (string)/itarator.cpp
@@ -10,9 +10,13 @@ int main()
     // }
     // cout<<*s.begin()<<endl;
     // cout<<*(s.end()-1)<<endl;
-    for (string :: iterator it = s.begin(); it < s.end(); it++)
+    for (auto it = s.begin(); it < s.end(); it++)
     {
         cout << *it << endl;
     }
+    // for (string :: iterator it = s.begin(); it < s.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-class MyClass {
-public:
-    char z; //1 byte
-    int x;  //4 byte
-    double y;   //8 byte
-};
-
-int main() {
-    MyClass obj;
-    
-    cout<<sizeof(obj.x)<<endl;
-    cout<<sizeof(obj.y)<<endl;
-    cout<<sizeof(obj.z)<<endl;
-    cout<<sizeof(obj);
+int main()
+{
+    string s(5,'a');
+    cout<<s;
     return 0;
 }
