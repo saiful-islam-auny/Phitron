014eebea88cf6202d4bcc7607b2b6c3a9c04e769
saiful-islam-auny <aunychowdhury99@gmail.com>
2023-11-02 22:37:36 +0600

    HW day 1

---
+++ Python/Day 1 (basic)/HW.py
@@ -0,0 +1,69 @@
+""" HOMEWORK
+
+1. Float
+2. take 3 numbers from the user and give me the largest number as output
+3. take 3 numbers from the use and give me the sum of the numbers
+4. run a loop and show  the odd numbers between 39 to 68
+5. Grade calculator in python (if elif else)
+
+ """
+
+# 1
+# f = input("Your CGPA: ")
+# f = float(f)
+# print("Congrats! it's your CGPA:",f)
+
+
+# 2
+# num1 = input("Enter number 1: ")
+# num2 = input("Enter number 2: ")
+# num3 = input("Enter number 3: ")
+
+# if num1>num2 and num1>num3:
+#     big = num1
+# elif num2>num1 and num2>num3:
+#     big = num2
+# else:
+#     big = num3
+
+# print("Biggest number:",big)
+
+
+# 3
+# num1 = input("Enter number 1: ")
+# num2 = input("Enter number 2: ")
+# num3 = input("Enter number 3: ")
+
+# num1 = int(num1)
+# num2 = int(num2)
+# num3 = int(num3)
+
+# print("Sum:",num1+num2+num3)
+
+
+# 4
+# for i in range(39,68):
+#     if i%2!=0:
+#         print(i)
+
+
+#5
+
+# mark = input("Enter your mark: ")
+# mark = float(mark)
+
+# if mark>=80:
+#     print('A+')
+# elif mark>=70:
+#     print('A')
+# elif mark>=60:
+#     print('A-')
+# elif mark>=50:
+#     print('B')
+# elif mark>=40:
+#     print('C')
+# elif mark>=33:
+#     print('D')
+# else:
+#     print('F')
+
